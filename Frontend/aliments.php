91c949e2d42efa5c3edc3f3a94dc8b784e27d1ce
Lil119 <d.petitcolas@outlook.fr>
2023-10-27 17:19:39 +0200

    aliments ++

--- Frontend/aliments.php
+++ Frontend/aliments.php
@@ -1,23 +1,66 @@
 <!-- FORMULAIRE POUR RENSEIGNER UN NOUVEL ALIMENT -->
-
 <div class="box" id="box_form_conso">
     <h1>Ajout d'un nouvel aliment au catalogue</h1>
     <form>
-        <div class="mb-3">
-            <label class="form-label">Nom</label>
-            <input type="text" class="form-control" id="inputAliment">
-            <label class="form-label">Quantite</label>
-            <input type="text" class="form-control" id="inputQuantite">
-            <label class="form-label">Portion</label>
-            <input type="text" class="form-control" id="inputPortion">
-            <label class="form-label">Marque</label>
-            <input type="text" class="form-control" id="inputMarque">
-            <label class="form-label">Energie</label>
-            <input type="text" class="form-control" id="inputEnergie">
-            <label class="form-label">Unité de l'energie</label>
-            <input type="text" class="form-control" id="inputUnite">
+        <div class="row">
+            <div class="col-6">
+                <label class="form-label">Nom</label>
+                <input type="text" class="form-control" id="inputAliment">
+                <div class="form-label">Quantite</div>
+                <input type="text" class="form-control" id="inputQuantite">
+                <div class="form-label">Portion</div>
+                <input type="text" class="form-control" id="inputPortion">
+                <div class="form-label">Marque</div>
+                <input type="text" class="form-control" id="inputMarque">
+            </div>
+            <div class="col-6">
+                <label class="form-label">Energie</label>
+                <input type="text" class="form-control" id="inputEnergie">
+                <label class="form-label">Unité de l'energie</label>
+                <input type="text" class="form-control" id="inputUnite">
+                <label class="form-label">Nutriscore</label>
+                <input type="text" class="form-control" id="inputNutriscore">
+            </div>
         </div>
-        <button type="submit" class="btn btn-primary body-button">Valider</button>
+        <div class="encadre">
+            <table id="table">
+                <tr>
+                    <td>
+                        <input type="text" class="form-control input-taille" placeholder="Ingrédient" id="inputIngredient1">
+                    </td>
+                    <td>
+                        <input type="text" class="form-control input-taille" placeholder="Pourcentage" id="inputPourcentage1">
+                    </td>
+                    <td>
+                        <button class="btn btn-danger" onclick="supprimerLigne(this)">Supprimer</button>
+                    </td>
+                </tr>
+            </table>
+            <input type="button" class="btn btn-primary" id="inputAjoutIng" onclick="ajouterLigne()" value="Ajouter une ligne">
+        </div>
+        <div class="encadre">
+            <table id="tableNutriment">
+                <tr>
+                    <td>
+                        <input type="text" class="form-control input-taille" placeholder="Nutriment" id="inputNutriment1">
+                    </td>
+                    <td>
+                        <input type="text" class="form-control input-taille" placeholder="Quantité pour 100g" id="inputQuantite_1001">
+                    </td>
+                    <td>
+                        <input type="text" class="form-control input-taille" placeholder="Quantité pour une portion" id="inputQuantite_portion1">
+                    </td>
+                    <td>
+                        <input type="text" class="form-control input-taille" placeholder="Unité des quantités" id="inputQuantite_unite1">
+                    </td>
+                    <td>
+                        <button class="btn btn-danger" onclick="supprimerLigne(this)">Supprimer</button>
+                    </td>
+                </tr>
+            </table>
+            <input type="button" class="btn btn-primary" id="inputAjoutNut" onclick="ajouterLigneNutriment()" value="Ajouter une ligne">
+        </div>
+        <button type="submit" class="btn btn-success body-button">Valider</button>
     </form>
 </div>
 
@@ -28,7 +71,13 @@
     <table class="table" id="table_aliments">
         <thead>
             <tr>
+                <th scope="col">Code</th>
                 <th scope="col">Nom</th>
+                <th scope="col">Quantité</th>
+                <th scope="col">Portion</th>
+                <th scope="col">Marque</th>
+                <th scope="col">Energie</th>
+                <th scope="col">Nutriscore</th>
                 <th scope="col">CRUD</th>
             </tr>
         </thead>
@@ -39,5 +88,4 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-
 <script src="js/script_aliments.js"></script>
--- Frontend/js/script_aliments.js
+++ Frontend/js/script_aliments.js
@@ -1,11 +1,36 @@
 $(document).ready(function () {
+    //Initialisation de la table des aliments
     let table = new DataTable('#table_aliments', {
         columnDefs: [{
             targets: 0,
-            data: 'Nom'
+            data: 'Code'
         },
         {
             targets: 1,
+            data: 'Nom'
+        },
+        {
+            targets: 2,
+            data: 'Quantite'
+        },
+        {
+            targets: 3,
+            data: 'Portion'
+        },
+        {
+            targets: 4,
+            data: 'Marque'
+        },
+        {
+            targets: 5,
+            data: 'Energie'
+        },
+        {
+            targets: 6,
+            data: 'Nutriscore'
+        },
+        {
+            targets: 7,
             data: null,
             render: function (data, type, row) {
                 return '<button id="edit" onclick="onEditSubmit();">Éditer</button><button id="delete" onclick="onDeleteSubmit();">Supprimer</button>';
@@ -13,4 +38,76 @@ $(document).ready(function () {
         }
         ]
     });
+
 });
+
+
+//Gestion de l'interface ajouter/supprimer pour les ingrédients
+let ligneCount = 1;
+
+function ajouterLigne() {
+    ligneCount++;
+
+    const table = document.getElementById("table");
+    const newRow = table.insertRow(table.rows.length);
+
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    const cell3 = newRow.insertCell(2);
+
+    cell1.innerHTML = `
+        <input type="text" class="form-control input-taille" placeholder="Ingrédient" id="inputIngredient${ligneCount}">
+`;
+    cell2.innerHTML = `
+        <input type="text" class="form-control input-taille" placeholder="Pourcentage" id="inputPourcentage${ligneCount}">
+`;
+
+    cell3.innerHTML = `
+    <button class="btn btn-danger" onclick="supprimerLigne(this)">Supprimer</button>
+`;
+}
+
+function supprimerLigne(button) {
+    const row = button.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
+
+
+
+//Gestion de l'interface ajouter/supprimer pour les nutriments
+let ligneCountNutriment = 1;
+
+function ajouterLigneNutriment() {
+    ligneCountNutriment++;
+
+    const table = document.getElementById("tableNutriment");
+    const newRow = table.insertRow(table.rows.length);
+
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    const cell3 = newRow.insertCell(2);
+    const cell4 = newRow.insertCell(3);
+    const cell5 = newRow.insertCell(4);
+
+    cell1.innerHTML = `
+    <input type="text" class="form-control input-taille" placeholder="Nutriment" id="inputNutriment${ligneCount}">
+`;
+    cell2.innerHTML = `
+    <input type="text" class="form-control input-taille" placeholder="Quantité pour 100g" id="inputQuantite_100${ligneCount}">
+`;
+
+    cell3.innerHTML = `
+    <input type="text" class="form-control input-taille" placeholder="Quantité pour une portion" id="inputQuantite_portion${ligneCount}">
+`;
+    cell4.innerHTML = `
+    <input type="text" class="form-control input-taille" placeholder="Unité des quantités" id="inputQuantite_unite${ligneCount}">
+`;
+    cell5.innerHTML = `
+    <button class="btn btn-danger" onclick="supprimerLigne(this)">Supprimer</button>
+`;
+}
+
+function supprimerLigneNutriment(button) {
+    const row = button.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
