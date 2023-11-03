425951e4269d9e1485ff9dc49f140598d0232b77
Lil119 <d.petitcolas@outlook.fr>
2023-11-03 14:50:09 +0100

    création compte

--- Frontend/js/script_login.js
+++ Frontend/js/script_login.js
@@ -1,6 +1,7 @@
 //Vérification du mot de passe
 $('#btnValider').on("click", function (e) {
     let identifiant = $("#inputLogin").val();
+    let mdpSaisi = $("#inputMdp").val();
 
     $.ajax({
         url: URL_START + 'Backend/utilisateurs.php?identifiant=' + identifiant,
@@ -8,6 +9,13 @@ $('#btnValider').on("click", function (e) {
         dataType: 'json',
         success: function (data) {
             console.log(data);
+            if (password_verify($mdpSaisi, data.MOT_DE_PASSE)) {
+                // Le mot de passe est correct
+                console.log('Le mot de passe est correct');
+            } else {
+                // Le mot de passe est incorrect
+                console.log('Le mot de passe est incorrect');
+            }
         },
         error: function (error) {
             console.error('Erreur lors de la récupération des données : ', error);
