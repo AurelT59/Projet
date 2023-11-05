cd71d09187217386f437e15711f61df22afb6231
AurelT59 <aurelien.terrades@gmail.com>
2023-11-05 22:32:20 +0100

    aliments gestion de la reponse

--- Backend/aliments.php
+++ Backend/aliments.php
@@ -119,25 +119,39 @@ switch ($_SERVER['REQUEST_METHOD']) {
 
 
 // gestion de la réponse à la requête
-function checkAndResponse($request, $result, $data)
+function checkAndResponse($request, $data)
 {
+    if (is_bool($request)) {
 
-    if (($result == null && $_SERVER['REQUEST_METHOD'] == 'GET') | ($request == false && $_SERVER['REQUEST_METHOD'] != 'POST')) {
-        // Aucune donnée trouvée, renvoyer un statut 404 - Not Found
-        http_response_code(404);
-        echo json_encode(array('message' => "Aucun aliment trouvé."));
-    } else if ($request == true || $request != 0) {
-        // Les données ont été récupérées avec succès, renvoyer un statut 200 - OK
-        http_response_code(200);
-        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-            echo json_encode($result);
-        } else {
-            echo json_encode(array('code' => $data['code']));
-        }
-    } else {
         // Erreur de la requête SQL, renvoyer un statut 500 - Internal Server Error
         http_response_code(500);
-        echo json_encode(array('message' => "Erreur interne du serveur."));
+        echo json_encode(array('message' => "Echec de la requête SQL : Erreur interne au serveur."));
+    } else {
+
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $result = $request->fetchAll(PDO::FETCH_OBJ);
+        }
+
+        if ((empty($result) && $_SERVER['REQUEST_METHOD'] == 'GET') || ($request == 0)) {
+
+            // Aucune donnée trouvée, renvoyer un statut 404 - Not Found
+            http_response_code(404);
+            echo json_encode(array('message' => "Aucun utilisateur trouvé."));
+        } else {
+
+            // Les données ont été récupérées avec succès, renvoyer un statut 200 - OK
+            http_response_code(200);
+
+            if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+                echo json_encode($result);
+            } else {
+                if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+                    echo json_encode(array('identifiant' => $data['identifiant_nouveau']));
+                } else {
+                    echo json_encode(array('identifiant' => $data['identifiant']));
+                }
+            }
+        }
     }
 }
 
